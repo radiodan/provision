4d2a760bf4aa6d9f36f350f5acbd44f2794869ea
Dan Nuttall <pixelblend@gmail.com>
2014-03-05 17:55:33 +0000

    Seperate setup into prepare & install

--- install.sh
+++ install.sh
@@ -6,20 +6,6 @@
   mkdir -p ${RADIODAN_FS} && \
     curl -L https://github.com/pixelblend/provision/archive/master.tar.gz | tar xz --strip-components 1 -C ${RADIODAN_FS}
 
-# clean up distro
-  apt-get update && \
-    apt-get purge -y x11-common lxde midori scratch && \
-    apt-get autoremove -y
-
-# remove x11 stuff
-  update-rc.d -f x11-common remove
-
-# update firmware
-  apt-get install -y rpi-update && rpi-update
-
-# Upstart
-  yes 'Yes, do as I say!' | apt-get -y --force-yes install upstart
-
 # TODO: Add speed hacks inc. tmpfs
   cp ${RADIODAN_FS}/prepare-dirs /etc/init.d/prepare-dirs && \
     chmod +x /etc/init.d/prepare-dirs && \
