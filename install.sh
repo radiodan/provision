ccd272fa524c7bc164f84b2f4d8b45bcf1638a00
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-04-17 15:08:04 +0100

    Update download URLs

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@
 
 # Install physical UI
   mkdir -p /opt/radiodan/buttons/ && \
-    curl -L https://github.com/radiodan/physical-ui/releases/download/v0.0.2/buttons.tar.gz | tar xz --strip-components 1 -C /opt/radiodan/buttons && \
+    curl -L https://github.com/radiodan/physical-ui/releases/download/v0.0.1/radiodan-buttons.tar.gz | tar xz --strip-components 1 -C /opt/radiodan/buttons && \
     cp -v ${RADIODAN_CONF}/radiodan-buttons.conf /etc/init
 
 # Tidying Up
