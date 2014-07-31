7a4fb459964811ab8c441df65a90de84f656f88a
Dan Nuttall <pixelblend@gmail.com>
2014-07-31 15:01:43 +0100

    Create adhoc directory during nginx configuration

--- steps/nginx/configure.sh
+++ steps/nginx/configure.sh
@@ -1,3 +1,4 @@
+mkdir -pv /opt/radiodan/adhoc/ && \
     cp -v ${RADIODAN_CONF}/wpa_cli_web_redirect /etc/nginx/sites-available/wpa_cli_web_redirect && \
     cp -v ${RADIODAN_CONF}/radiodan_client /etc/nginx/sites-available/radiodan_client && \
     cp -v ${RADIODAN_CONF}/status511.html /opt/radiodan/adhoc/status511.html && \
