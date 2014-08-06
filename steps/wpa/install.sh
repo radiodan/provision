d5e19744a0d6d3980702296a283fa222d0cc02d0
Dan Nuttall <pixelblend@gmail.com>
2014-08-06 17:17:14 +0100

    Update wpa step

--- steps/wpa/install.sh
+++ steps/wpa/install.sh
@@ -1,4 +1,1 @@
-apt-get install -y --force-yes dnsmasq && \
-  apt-get install -y hostapd wpasupplicant && \
-  apt-get install -y ruby1.9.3 ruby-dev && \
-  gem install --no-ri --no-rdoc wpa_cli_web foreman procfile-upstart-exporter
+gem install --no-ri --no-rdoc wpa_cli_web foreman procfile-upstart-exporter
