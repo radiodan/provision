56c96a3215cb58063ece6fb62ff98f90bd300247
Dan Nuttall <pixelblend@gmail.com>
2014-08-26 14:51:03 +0100

    Move physical ui server to init.d

--- steps/radiodan-physical-ui/configure.sh
+++ steps/radiodan-physical-ui/configure.sh
@@ -1,2 +1,3 @@
-    cp -v ${RADIODAN_CONF}/radiodan-buttons.conf /etc/init && \
-    cp -v /opt/radiodan/magic/config/physical-ui-config.json.example /opt/radiodan/magic/config/physical-ui-config.json
+cp -v ${RADIODAN_CONF}/radiodan-buttons /etc/init.d && \
+  update-rc.d radiodan-buttons defaults && \
+  cp -v /opt/radiodan/magic/config/physical-ui-config.json.example /opt/radiodan/magic/config/physical-ui-config.json
