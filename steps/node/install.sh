709a5361ec30a647737ccca89a8bcc7dfef6c98c
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-09-03 12:33:54 +0100

    Do not install forever

--- steps/node/install.sh
+++ steps/node/install.sh
@@ -2,5 +2,4 @@
     $(curl -L http://nodejs.org/dist/v0.10.24/node-v0.10.24-linux-arm-pi.tar.gz | tar xz --strip-components 1 -C /opt/node) && \
     ln -sf /opt/node/bin/node /usr/local/bin/node && \
     ln -sf /opt/node/bin/npm /usr/local/bin/npm && \
-    /opt/node/bin/npm config set prefix /usr/local && \
-    /opt/node/bin/npm -g install forever
+    /opt/node/bin/npm config set prefix /usr/local
