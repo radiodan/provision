cfd417d27ebb2f8ba87a2a94c74418cb5be4e146
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-09-09 15:00:46 +0100

    Convenience script to change device services

--- steps/monit/configure.sh
+++ steps/monit/configure.sh
@@ -1,3 +1,8 @@
-mkdir -pv /etc/monit/{conf.d,monitrc.d}/ && \
-    cp -v ${RADIODAN_CONF}/radiodan /etc/monit/monitrc.d/ && \
-    ln -s /etc/monit/monitrc.d/radiodan /etc/monit/conf.d/radiodan
+mkdir -pv /etc/monit/conf.d/ && \
+    mkdir -pv /etc/monit/monitrc.d/ && \
+    cp -v ${RADIODAN_CONF}/general /etc/monit/monitrc.d/ && \
+    cp -v ${RADIODAN_CONF}/radiodan-example /etc/monit/monitrc.d/ && \
+    cp -v ${RADIODAN_CONF}/radiodan-magic /etc/monit/monitrc.d/ && \
+    ln -sf /etc/monit/monitrc.d/general /etc/monit/conf.d/general && \
+    ln -sf /etc/monit/monitrc.d/radiodan-magic /etc/monit/conf.d/radiodan-magic && \
+    cp -v ${RADIODAN_CONF}/radiodan-device-type /usr/local/bin/
