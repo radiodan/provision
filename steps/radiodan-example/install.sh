fcdfe38d478233783749cc8b6e455641276db55a
Andrew Nicolaou <mail@andrewnicolaou.co.uk>
2014-09-10 10:51:55 +0100

    Install example app with mediascape discovery

--- steps/radiodan-example/install.sh
+++ steps/radiodan-example/install.sh
@@ -1,2 +1,2 @@
 mkdir -pv /opt/radiodan/example && \
-  curl -L http://vm-1027-user.virt.ch.bbc.co.uk/releases/radiodan/client-web-example/master | tar xz -C /opt/radiodan/example
+  curl -L http://vm-1027-user.virt.ch.bbc.co.uk/releases/radiodan/client-web-example/mediascape | tar xz -C /opt/radiodan/example
