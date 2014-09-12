b1af5f11e7f690ae746c79f49ba0fb5569c546a1
Libby Miller <libby@nicecupoftea.org>
2014-09-12 10:40:29 +0000

    Add LSB tags for radiodan-* and wpa, and (for wpa) ruby1.9.1-dev

--- steps/wpa/install.sh
+++ steps/wpa/install.sh
@@ -1,3 +1,3 @@
 apt-get install -y --force-yes dnsmasq && \
-  apt-get install -y ruby2.1 ruby2.1-dev hostapd wpasupplicant && \
+  apt-get install -y ruby2.1 ruby2.1-dev ruby1.9.1-dev hostapd wpasupplicant && \
   gem install --no-ri --no-rdoc wpa_cli_web
