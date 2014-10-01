146c0bbcd3eae2f7aa0a3b7d6ef3dc99a952aef1
Dan Nuttall <pixelblend@gmail.com>
2014-10-01 11:49:42 +0100

    Fix ssh name, permissions

--- steps/ssh/configure.sh
+++ steps/ssh/configure.sh
@@ -1,2 +1,4 @@
 mkdir -pv /home/pi/.ssh && \
-  cat ${RADIODAN_CONF}/id_radiodan.pub >> /home/pi/.ssh/authorised_keys
+  cat ${RADIODAN_CONF}/id_radiodan.pub >> /home/pi/.ssh/authorized_keys && \
+  chown pi:pi /home/pi/.ssh/authorized_keys && \
+  chmod 600 /home/pi/.ssh/authorized_keys
