2f77026d57fc548f126d7e18f3f537ad1922c53d
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-10-10 10:55:15 +0100

    Install cease using updater

--- steps/radiodan-cease/install.sh
+++ steps/radiodan-cease/install.sh
@@ -1,3 +1,4 @@
-mkdir -pv /opt/radiodan/apps/cease/releases/0
-curl -L http://deploy.radiodan.net/releases/radiodan/cease/master | tar xz -C /opt/radiodan/apps/cease/releases/0
-ln -sf /opt/radiodan/apps/cease/releases/0 /opt/radiodan/apps/cease/current
+mkdir -pv /opt/radiodan/apps/cease/releases/0 && \
+  cp -v ${RADIODAN_CONF}/deploy /opt/radiodan/apps/cease/releases/0/.deploy && \
+  rm -f /opt/radiodan/apps/cease/current && \
+  ln -s /opt/radiodan/apps/cease/releases/0 /opt/radiodan/apps/cease/current
