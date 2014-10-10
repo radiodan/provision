afeb7b3320662e8661615b90f9f6d5f79214dcad
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-10-10 10:55:36 +0100

    Fix bug where cease configure step fails

--- steps/radiodan-cease/configure.sh
+++ steps/radiodan-cease/configure.sh
@@ -1,3 +1,2 @@
-cp -v ${RADIODAN_CONF}/radiodan-cease /etc/init.d
-update-rc.d radiodan-cease defaults
-/etc/init.d/radiodan-cease start
+cp -v ${RADIODAN_CONF}/radiodan-cease /etc/init.d  && \
+  update-rc.d radiodan-cease defaults
