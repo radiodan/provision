b3e31180520f412e0500cdeba1b7e416498c5abe
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-10-10 10:35:30 +0100

    Monitor cease process with monit

--- steps/monit/configure.sh
+++ steps/monit/configure.sh
@@ -1,10 +1,10 @@
 mkdir -pv /etc/monit/conf.d/ && \
     mkdir -pv /etc/monit/monitrc.d/ && \
     cp -v ${RADIODAN_CONF}/general /etc/monit/monitrc.d/ && \
-    cp -v ${RADIODAN_CONF}/radiodan-updater /etc/monit/monitrc.d/ && \
+    cp -v ${RADIODAN_CONF}/radiodan-system /etc/monit/monitrc.d/ && \
     cp -v ${RADIODAN_CONF}/radiodan-type-example /etc/monit/monitrc.d/ && \
     cp -v ${RADIODAN_CONF}/radiodan-type-magic /etc/monit/monitrc.d/ && \
     ln -sf /etc/monit/monitrc.d/general /etc/monit/conf.d/general && \
-    ln -sf /etc/monit/monitrc.d/radiodan-updater /etc/monit/conf.d/radiodan-updater && \
-    ln -sf /etc/monit/monitrc.d/radiodan-type-magic /etc/monit/conf.d/radiodan-type-magic && \
+    ln -sf /etc/monit/monitrc.d/radiodan-system /etc/monit/conf.d/ && \
+    ln -sf /etc/monit/monitrc.d/radiodan-type-magic /etc/monit/conf.d/ && \
     cp -v ${RADIODAN_CONF}/radiodan-device-type /usr/local/bin/
