01f3f0424fde5cb18b0f72869332da6c56845708
Dan Nuttall <pixelblend@gmail.com>
2014-11-14 10:48:08 +0000

    Add pi user to samba share.
    Default password = pi

--- steps/samba/configure.sh
+++ steps/samba/configure.sh
@@ -1,5 +1,7 @@
   mkdir -pv /media/music && chmod 777 -R /media/music && \
   mkdir -pv /opt/radiodan && chown 755 -R /opt/radiodan
 
+  echo -ne "pi\npi\n" | smbpasswd -a -s pi
+
   cp -v ${RADIODAN_CONF}/smb.conf /etc/samba/smb.conf && \
     service samba restart
