2b24b02b42ae594a34c4020d5640d8da666fd501
Dan Nuttall <pixelblend@gmail.com>
2014-11-18 11:31:05 +0000

    Add user after configuring samba service

--- steps/samba/configure.sh
+++ steps/samba/configure.sh
@@ -1,7 +1,9 @@
-  mkdir -pv /media/music && chmod 777 -R /media/music && \
-  mkdir -pv /opt/radiodan && chown 755 -R /opt/radiodan
+mkdir -pv /media/music && chmod 777 -R /media/music
+mkdir -pv /opt/radiodan && chown 755 -R /opt/radiodan
 
-  echo -ne "pi\npi\n" | smbpasswd -a -s pi
+cp -v ${RADIODAN_CONF}/smb.conf /etc/samba/smb.conf
+service samba restart
 
-  cp -v ${RADIODAN_CONF}/smb.conf /etc/samba/smb.conf && \
-    service samba restart
+echo -ne "pi\npi\n" | smbpasswd -a -s pi
+service samba restart
+
