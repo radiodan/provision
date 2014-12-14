efa2e55281350c4d1a5a9a3a9d9ce45ca49ecafc
Libby Miller <libby@nicecupoftea.org>
2014-12-14 16:30:43 +0000

    Fix path of status511.html file

--- steps/nginx/configure.sh
+++ steps/nginx/configure.sh
@@ -3,5 +3,5 @@ cp -v ${RADIODAN_CONF}/wpa_cli_web_redirect /etc/nginx/sites-available/wpa_cli_w
 cp -v ${RADIODAN_CONF}/radiodan_client /etc/nginx/sites-available/radiodan_client
 cp -v ${RADIODAN_CONF}/status511.html /opt/radiodan/static/status511.html
 cp -v ${RADIODAN_CONF}/radiodan404.html /opt/radiodan/static/radiodan404.html
-chown 755 /opt/radiodan/adhoc/status511.html
+chown 755 /opt/radiodan/static/status511.html
 rm -vf /etc/nginx/sites-enabled/default
