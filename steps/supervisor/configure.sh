ad22cf8e6e7d9b8da8449300e3a0a10ddda10dd0
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-04-30 15:47:18 +0100

    Use supervisor to manage proceses and cron to check for updates

---
+++ steps/supervisor/configure.sh
@@ -0,0 +1,9 @@
+mkdir -pv /etc/supervisor/conf.d/ && \
+  mkdir -pv /etc/supervisor/available/ && \
+    cp -v ${RADIODAN_CONF}/radiodan-system /etc/supervisor/available/radiodan-system.conf && \
+    cp -v ${RADIODAN_CONF}/radiodan-type-example /etc/supervisor/available/radiodan-type-example.conf && \
+    cp -v ${RADIODAN_CONF}/radiodan-type-magic /etc/supervisor/available/radiodan-type-magic.conf && \
+    ln -sf /etc/supervisor/available/general.conf /etc/supervisor/conf.d/ && \
+    ln -sf /etc/supervisor/available/radiodan-system.conf /etc/supervisor/conf.d/ && \
+    ln -sf /etc/supervisor/available/radiodan-type-magic.conf /etc/supervisor/conf.d/ && \
+    cp -v ${RADIODAN_CONF}/radiodan-device-type /usr/local/bin/
