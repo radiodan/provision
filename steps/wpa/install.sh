76bb394a9b0058d93018e8ff1241907e30fa79e2
libbymiller <libby@nicecupoftea.org>
2015-05-19 12:22:42 +0100

    Updating after a security update to hostapd

--- steps/wpa/install.sh
+++ steps/wpa/install.sh
@@ -1,3 +1,3 @@
 apt-get install -y --force-yes dnsmasq && \
-  apt-get install -y --force-yes ruby2.1 ruby2.1-dev ruby1.9.1-dev hostapd=1:1.0-3+deb7u1 wpasupplicant && \
+  apt-get install -y --force-yes ruby2.1 ruby2.1-dev ruby1.9.1-dev hostapd=1:1.0-3+deb7u2 wpasupplicant && \
   gem install --no-ri --no-rdoc wpa_cli_web
