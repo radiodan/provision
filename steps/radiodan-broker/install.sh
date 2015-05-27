3390459f8b852c1541fd15ee3afe6c6db74820c0
Dan Nuttall <pixelblend@gmail.com>
2015-05-27 11:08:07 +0100

    Start broker as part of radiodan-system group

--- steps/radiodan-broker/install.sh
+++ steps/radiodan-broker/install.sh
@@ -1,3 +1,3 @@
 mkdir -pv /opt/radiodan/apps/broker/releases/0 && \
-  cp -v ${RADIODAN_CONF}/deploy /opt/radiodan/apps/example/releases/0/.deploy && \
-  ln -sf /opt/radiodan/apps/example/releases/0 /opt/radiodan/apps/example/current
+  cp -v ${RADIODAN_CONF}/deploy /opt/radiodan/apps/broker/releases/0/.deploy && \
+  ln -sf /opt/radiodan/apps/broker/releases/0 /opt/radiodan/apps/broker/current
