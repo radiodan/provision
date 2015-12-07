52e77506ddca13fe4e061ae381bd5368bbdfc5fa
Libby Miller <libby@nicecupoftea.org>
2015-12-07 22:42:54 +0000

    Fix provision for jessie

--- steps/wpa/install.sh
+++ steps/wpa/install.sh
@@ -1,3 +1,7 @@
 apt-get install -y --force-yes dnsmasq && \
-  apt-get install -y --force-yes ruby2.1 ruby2.1-dev ruby1.9.1-dev hostapd=1:1.0-3+deb7u2 wpasupplicant && \
+  apt-get install -y --force-yes ruby2.1-dev && \
+  curl -O http://old-releases.ubuntu.com/ubuntu/pool/universe/w/wpa/hostapd_1.0-3ubuntu2.1_armhf.deb && \
+  sudo dpkg -i hostapd_1.0-3ubuntu2.1_armhf.deb && \
+  apt-get install -y --force-yes wpasupplicant && \
   gem install --no-ri --no-rdoc wpa_cli_web
+
