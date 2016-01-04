c96bcece14b7955978475f6300de0f531a0879e0
Libby Miller <libby@nicecupoftea.org>
2016-01-04 21:35:13 +0000

    INstall by hand

--- steps/radiodan-server/install.sh
+++ steps/radiodan-server/install.sh
@@ -1,4 +1,6 @@
 mkdir -pv /opt/radiodan/apps/server/releases/0 && \
-  cp -v ${RADIODAN_CONF}/deploy /opt/radiodan/apps/server/releases/0/.deploy && \
+  git clone https://github.com/radiodan/radiodan.js  /opt/radiodan/apps/server/releases/0 && \
+  mkdir -pv /opt/radiodan/apps/server/releases/0/node_modules && \
+  npm install --prefix /opt/radiodan/apps/server/releases/0/node_modules && \
   rm -f /opt/radiodan/apps/server/current && \
   ln -s /opt/radiodan/apps/server/releases/0 /opt/radiodan/apps/server/current
--- steps/radiodan-skeleton/install.sh
+++ steps/radiodan-skeleton/install.sh
@@ -1,4 +1,6 @@
 mkdir -pv /opt/radiodan/apps/skeleton/releases/0 && \
-  cp -v ${RADIODAN_CONF}/deploy /opt/radiodan/apps/skeleton/releases/0/.deploy && \
+  git clone https://github.com/radiodan/radiodan-skeleton  /opt/radiodan/apps/skeleton/releases/0 && \
+  mkdir -pv /opt/radiodan/apps/skeleton/releases/0/node_modules && \
+  npm install --prefix /opt/radiodan/apps/skeleton/releases/0/node_modules && \
   rm -f /opt/radiodan/apps/skeleton/current && \
   ln -s /opt/radiodan/apps/skeleton/releases/0 /opt/radiodan/apps/skeleton/current
