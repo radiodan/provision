0100c2cfa4fe2fdc960b196461b14a5fdfdb4bae
Libby Miller <libby@nicecupoftea.org>
2016-01-04 19:22:34 +0000

    Fix samba install errors

--- steps/samba/configure.sh
+++ steps/samba/configure.sh
@@ -2,8 +2,8 @@ mkdir -pv /media/music && chmod 777 -R /media/music
 mkdir -pv /opt/radiodan && chown 755 -R /opt/radiodan
 
 cp -v ${RADIODAN_CONF}/smb.conf /etc/samba/smb.conf
-service samba restart
+service smbd restart
 
 /usr/bin/yes pi | smbpasswd -a -s pi
-service samba restart
+service smbd restart
 
