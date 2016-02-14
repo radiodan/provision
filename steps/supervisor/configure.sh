7f2f5230544698ffd44d241439b23753c2ef0d11
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2016-02-14 16:00:07 +0100

    Add helper to export Procfile to supervisor config

--- steps/supervisor/configure.sh
+++ steps/supervisor/configure.sh
@@ -1,2 +1,6 @@
 mkdir -pv /etc/supervisor/conf.d/ && \
-  cp -v ${RADIODAN_CONF}/general /etc/supervisor/conf.d/general.conf
+  cp -v ${RADIODAN_CONF}/general /etc/supervisor/conf.d/general.conf && \
+  npm install -g foreman && \
+  cp -v ${RADIODAN_CONF}/radiodan-default-app /usr/local/bin/radiodan-default-app && \
+  chmod +x /usr/local/bin/radiodan-default-app && \
+  radiodan-default-app skeleton
