69d459d6a51ac870cc8ffd9bedf648f48786fcc5
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2016-04-02 14:46:52 +0200

    Install latest nodejs armv6

--- steps/node/install.sh
+++ steps/node/install.sh
@@ -1,5 +1,5 @@
   mkdir -pv /opt/node && \
-    $(curl -L http://nodejs.org/dist/v0.10.24/node-v0.10.24-linux-arm-pi.tar.gz | tar xz --strip-components 1 -C /opt/node) && \
+    $(curl -L https://nodejs.org/dist/v5.10.0/node-v5.10.0-linux-armv6l.tar.gz | tar xz --strip-components 1 -C /opt/node) && \
     ln -sf /opt/node/bin/node /usr/local/bin/node && \
     ln -sf /opt/node/bin/npm /usr/local/bin/npm && \
     /opt/node/bin/npm config set prefix /usr/local
