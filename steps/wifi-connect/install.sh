7f1ef2b36e1dea80aadd76927686e857f8136e1d
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2017-11-10 17:32:43 +0100

    Upgrades to resin-io/resin-wifi-connect v3
    v3 is based on NetworkManager and allows password-less access points
    so we don't need to maintain our patched fork of connman anymore. It
    also has an install script which simplifies our installation.
    
    `forget-wifi-networks` clears all previous connections and reboots
    the Pi. This is because as soon as this runs it will disconnect the
    wifi network that's connected.
    
    `start-wifi-connect` is a new script that Systemd will run on startup.
    This runs `wifi-connect` to see if any networks can be joined. After
    this if the device isn't connected to a network then it will run
    `wifi-connect --clear=true` to start the AP mode. Unfortunately this
    clears all existing wifi networks but this is the only way to enable
    AP mode using v3.

--- steps/wifi-connect/install.sh
+++ steps/wifi-connect/install.sh
@@ -1,17 +1,1 @@
-dpkg -i ${RADIODAN_CONF}/connman_1.21-1.2_armhf.deb && \
-  apt-get install -y -f \
-      bind9 \
-      bridge-utils \
-      iptables \
-      libdbus-1-dev \
-      libexpat-dev \
-      net-tools \
-      usbutils \
-      wireless-tools && \
-  mkdir -p /opt/radiodan && \
-  rm -rf /opt/radiodan/wifi-connect && \
-  git clone https://github.com/radiodan/resin-wifi-connect.git /opt/radiodan/wifi-connect && \
-  cd /opt/radiodan/wifi-connect/ && \
-  JOBS=MAX npm install --production && \
-  /opt/radiodan/wifi-connect/node_modules/.bin/bower --allow-root install && \
-  /opt/radiodan/wifi-connect/node_modules/.bin/coffee -c ./src
+curl https://raw.githubusercontent.com/resin-io/resin-wifi-connect/master/scripts/raspbian-install.sh | bash -s -- -y
