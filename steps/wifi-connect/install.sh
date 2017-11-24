576a2fa0e1bb265a01dd26074979ce2bcff37d2c
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2017-11-24 16:09:18 +0100

    Install radiodan/wifi-connect-ui over the vanilla web UI

--- steps/wifi-connect/install.sh
+++ steps/wifi-connect/install.sh
@@ -1,1 +1,12 @@
-curl https://raw.githubusercontent.com/resin-io/resin-wifi-connect/master/scripts/raspbian-install.sh | bash -s -- -y
+INSTALL_UI_DIR=/usr/local/share/wifi-connect/ui
+RELEASE_URL="https://api.github.com/repos/radiodan/wifi-connect-ui/releases/latest"
+
+download_regex='browser_download_url": "\K.*\.tar\.gz'
+
+curl https://raw.githubusercontent.com/resin-io/resin-wifi-connect/master/scripts/raspbian-install.sh | bash -s -- -y && \
+  download_url=$(curl "$RELEASE_URL" -s | grep -hoP "$download_regex") && \
+  download_dir=$(mktemp -d) && \
+  curl -Ls "$download_url" | tar -xz --strip-components 1 -C "$download_dir" && \
+  rm -r $INSTALL_UI_DIR && \
+  mkdir -p $INSTALL_UI_DIR && \
+  mv $download_dir/* $INSTALL_UI_DIR
